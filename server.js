8b928aa342d701fe68d1afa38b99d7643e02c092
Andrew <0954296607as@gmail.com>
2018-12-13 20:44:44 +0200

    second trying

--- server.js
+++ server.js
@@ -2,10 +2,7 @@
 const express = require('express');
 const path=require ('path');
 const socket=require('socket.io');
-
-const fs=require('fs');
-const sqlite=require('sqlite3');
-
+const baza=require('./baza');
 
 const pathPublic=path.join(__dirname, '/public');
 const app = express();
@@ -20,8 +17,37 @@ let list_of_users={};
 app.use(express.static(pathPublic));
 
 app.use(express.json());
-
-
+app.post('/system.html/price', (req, res)=>{
+        baza.getPrice(req.body.product,(err,data)=>{
+                if(err){console.error('There is a mistake in the price-'+err);}
+                        else{
+                        res.send(data);
+                        }
+                });
+});
+app.post('/system.html/collection',(req,res)=>{
+        baza.getCollection(req.body.product,(err,data)=>{
+                if(err){console.error('There is a mistake in the collection-'+err)}
+                        else{
+                                res.send(data);
+                        }
+        });
+});
+app.post('/system.html/recipes',(req,res)=>{
+        baza.getRecipes(req.body.product, req.body.color, (err,data)=>{
+                if(err){console.error("There is a mistake in the recipes-"+err)}
+                else{
+                        res.send(data);
+                }
+        })
+});
+app.get('/system.html/pigments', (req, res)=>{
+        //console.log(req.body.hello);
+        baza.get_price_pigments((data)=>{
+               res.send(data);
+        });
+});
+        
 
 let server= app.listen(port,'0.0.0.0',(err) => {
     if (err) {
@@ -38,14 +64,8 @@ io.on('connection',function (client){
     
     try{
         client.on('add users',(data)=>{
-
-
-//         list_of_users[id]=data;
-//           console.log(list_of_users);
-//            console.log(io.engine.clientsCount);
             client.emit('add users',list_of_users);
             list_of_users[id]=data;
-//            console.log(list_of_users);
             client.broadcast.emit('add users',{[id]: list_of_users[id]});
 
 
@@ -54,14 +74,12 @@ io.on('connection',function (client){
             client.broadcast.emit('message',data);
         });
         client.on('disconnect',()=>{
-//            console.log('client disconnected id='+list_of_users[id]+' id='+id);
             client.broadcast.emit('add users',{'delate':id});
             delete list_of_users[id];
-//            console.log(io.engine.clientsCount);
         });
     }catch (e){
     
-        console.log(e);
+        console.log("There is a mistake in the database-"+e);
         client.disconnect();
     }
 
